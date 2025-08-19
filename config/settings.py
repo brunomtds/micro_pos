33c0eef6e00164a677b3c28bd28ebfd4aac4d818
Bruno-VSCode <bruno.machadots@gmail.com>
2025-08-19 10:47:11 -0300

    not DEBUG STATIC_URL

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles" ) # Onde os arquivos estátic
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG:
+    STATIC_URL = f"https://{os.environ.get("RENDER_EXTERNAL_HOSTNAME" )}/static/"
